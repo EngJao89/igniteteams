cb785f0f73de32396b0a14b5842ad7c3aff7d743
EngJao89 <eng_joao@outlook.com>
2023-02-24 00:00:52 -0300

    feat(13-highlight): implementa componente de titulo e subtitulo

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,17 @@
 import { Container } from './styles';
+
 import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
 
 export function Groups() {
   return(
     <Container>
       <Header/>
+
+      <Highlight 
+        title="Turmas"
+        subtitle="jogue com sua turma"
+      />
     </Container>
   )
 }
