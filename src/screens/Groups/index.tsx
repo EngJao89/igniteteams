dfd0cbdf435bf0481cc519dbcba87530cb013476
EngJao89 <eng_joao@outlook.com>
2023-02-26 21:49:59 -0300

    feat(16-list-empty): implementa componente de lista vazia

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -5,11 +5,12 @@ import { FlatList } from 'react-native';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from '@components/ListEmpty';
 
 import { Container } from './styles';
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>(['Galera da Rocket', 'Amigos', 'CSGO']);
+  const [groups, setGroups] = useState<string[]>([]);
 
   return(
     <Container>
@@ -28,6 +29,12 @@ export function Groups() {
             title={item}
           />
         )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty 
+            message="Que tal cadastrar a primeira turma?"
+          />
+          )}
       />
     </Container>
   )
