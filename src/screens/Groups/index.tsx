07c4edfd9745322ce15bf4d86e74267d468cd255
EngJao89 <eng_joao@outlook.com>
2023-02-26 22:23:21 -0300

    feat(17-button): implementa componente de botão default na aplicação

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -6,6 +6,7 @@ import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
 import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 import { Container } from './styles';
 
@@ -34,7 +35,12 @@ export function Groups() {
           <ListEmpty 
             message="Que tal cadastrar a primeira turma?"
           />
-          )}
+        )}
+      />
+
+      <Button 
+        title='Criar nova turma'
+        type='PRIMARY'
       />
     </Container>
   )
