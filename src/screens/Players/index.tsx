e7633bb95cd13c9ce7fd083e610af6300045eb9a
EngJao89 <eng_joao@outlook.com>
2023-02-27 22:45:33 -0300

    feat(24-flatlist-horizontal): implementa lista horizontal de times

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,12 +1,18 @@
+import { useState } from 'react';
+import { FlatList, Alert, TextInput, Keyboard } from 'react-native'
+
 import { Input } from "@components/Input";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { ButtonIcon } from "@components/ButtonIcon";
 
-import { Container, Form } from "./styles";
+import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 import { Filter } from "@components/Filter";
 
 export function Players() {
+  const [team, setTeam] = useState('Time A');
+  const [players, setPlayers] = useState(['João', 'Vinycius', 'Crystiano', 'Mateus']);
+
   return(
     <Container>
       <Header showBackButton/>
@@ -27,9 +33,24 @@ export function Players() {
         />
       </Form>
 
-      <Filter 
-        title="Time A"
-      />
+      <HeaderList>
+        <FlatList 
+          data={['Time A', 'Time B']}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <Filter 
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+
+        <NumberOfPlayers>
+          {players.length}
+        </NumberOfPlayers>
+      </HeaderList>
     </Container>
   )
 }
