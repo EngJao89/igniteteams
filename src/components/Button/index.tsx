0bc5ab964567903ea9d0f417cc277c0474a8a2c0
EngJao89 <eng_joao@outlook.com>
2023-03-12 22:45:25 -0300

    refactor(50-button-upgrade): melhora botão da aplicação

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,21 +1,16 @@
-import { TouchableOpacity, TouchableOpacityProps } from 'react-native';
+import { TouchableOpacityProps } from "react-native";
 
-import { Container, Title, ButtonTypeStyleProps } from './styles';
+import { ButtonTypeStyleProps, Container, Title } from "./styles";
 
 type Props = TouchableOpacityProps & {
   title: string;
   type?: ButtonTypeStyleProps;
 }
 
-export function Button({title, type = 'PRIMARY', ...rest}: Props){
-  return(
-    <Container 
-      {...rest} 
-      type={type}
-    >
-      <Title>
-        {title}
-      </Title>
+export function Button({ title, type = 'PRIMARY', ...rest }: Props) {
+  return (
+    <Container type={type} {...rest}>
+      <Title>{title}</Title>
     </Container>
   )
 }
